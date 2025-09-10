40737837ad1787db282b05baf819b4dbbe7d7c48
Aayush Neupane <pfpaayups@gmail.com>
2025-09-10 16:33:20 +0545

    chore: initial ASP.NET Core MVC project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductInventoryManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
